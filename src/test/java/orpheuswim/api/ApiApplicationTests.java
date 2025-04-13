8cdad56ec4eec337f11fa8380a869112657e2b4e
Rodrigo Lima <rodrigolima.developer@gmail.com>
2025-04-13 09:21:46 -0300

    initial commit

---
+++ src/test/java/orpheuswim/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package orpheuswim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
