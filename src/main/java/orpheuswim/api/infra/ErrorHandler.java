b43f8b156dc27d282e3d672e66852cd466ee4c31
Rodrigo Lima <rodrigolima.developer@gmail.com>
2025-04-21 11:20:26 -0300

    error treatment done

--- src/main/java/orpheuswim/api/infra/ErrorHandler.java
+++ src/main/java/orpheuswim/api/infra/ErrorHandler.java
@@ -1,7 +1,10 @@
 package orpheuswim.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.ValidationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,5 +14,17 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity handleError404() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400(MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DataValidationError::new).toList());
+    }
+
+    private record DataValidationError(String field, String message){
+        public DataValidationError(FieldError err) {
+            this(err.getField(), err.getDefaultMessage());
+        }
     }
 }
